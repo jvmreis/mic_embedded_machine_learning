683596191d70ab45b89c517117a1afc40e137827
jvmreis <74378259+jvmreis@users.noreply.github.com>
2025-05-26 12:23:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mic_embedded_machine_learning
+using IMXRT1050 microphone with embedded machine learning
---
+++ mic_embedded_machine_learning/evkbimxrt1050_sai_peripheral/Debug/sources.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+C_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+S_SRCS := 
+S_UPPER_SRCS := 
+C_DEPS := 
+EXECUTABLES := 
+OBJS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+CMSIS/DSP/Source/BasicMathFunctions \
+CMSIS/DSP/Source/BayesFunctions \
+CMSIS/DSP/Source/CommonTables \
+CMSIS/DSP/Source/ComplexMathFunctions \
+CMSIS/DSP/Source/ControllerFunctions \
+CMSIS/DSP/Source/DistanceFunctions \
+CMSIS/DSP/Source/FastMathFunctions \
+CMSIS/DSP/Source/FilteringFunctions \
+CMSIS/DSP/Source/InterpolationFunctions \
+CMSIS/DSP/Source/MatrixFunctions \
+CMSIS/DSP/Source/QuaternionMathFunctions \
+CMSIS/DSP/Source/SVMFunctions \
+CMSIS/DSP/Source/StatisticsFunctions \
+CMSIS/DSP/Source/SupportFunctions \
+CMSIS/DSP/Source/TransformFunctions \
+CMSIS/DSP/Source/WindowFunctions \
+board \
+codec \
+codec/port \
+codec/port/wm8960 \
+component/gpio \
+component/i2c \
+component/lists \
+component/osa \
+component/uart \
+device \
+drivers \
+fatfs/source \
+fatfs/source/fsl_sd_disk \
+sdmmc/common \
+sdmmc/host/usdhc/non_blocking \
+sdmmc/osa \
+sdmmc/sd \
+source \
+startup \
+utilities/debug_console_lite \
+utilities \
+utilities/str \
+xip \
+
